b5263d7d6b1fc87108976f8b23e00358a05ecc63
surjanthakur <tsurjan506@gmail.com>
2026-02-26 01:37:55 +0530

    save

--- backend/src/router/posts_router.py
+++ backend/src/router/posts_router.py
@@ -1,1 +1,11 @@
 from fastapi import APIRouter, Depends, status, HTTPException
+from db.db_connection import get_session
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+post_router = APIRouter(prefix="/posts", tags=["posts"])
+
+
+# TODO: Implement the logic to fetch all posts from the database
+@post_router.get("/all", status_code=status.HTTP_200_OK)
+async def get_all_posts(session_db: AsyncSession = Depends(get_session)):
+    pass
