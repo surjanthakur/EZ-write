b853882a87609c0c2530711144bc6b108e26aa40
surjanthakur <tsurjan506@gmail.com>
2026-02-26 09:58:26 +0530

    save

---
+++ backend/src/router/users_router.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from db.db_connection import get_session
+from sqlmodel.ext.asyncio.session import AsyncSession
+from ..schemas.user import UserCreate, UserResponse
+
+
+user_router = APIRouter(prefix="/users", tags=["users"])
+
+
+# create account
+@user_router.post("/signup")
+async def create_account(
+    req_form: UserCreate, session_db: AsyncSession = Depends(get_session)
+):
+    pass
