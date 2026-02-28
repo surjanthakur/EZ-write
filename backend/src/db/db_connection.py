8c1b87110990de3a140cd13d93f7f939f117b07e
surjanthakur <tsurjan506@gmail.com>
2026-02-28 19:48:58 +0530

    save

--- backend/src/db/db_connection.py
+++ backend/src/db/db_connection.py
@@ -7,12 +7,10 @@ from typing import AsyncGenerator
 from sqlalchemy.exc import SQLAlchemyError
 from sqlmodel import SQLModel
 
-logger = logging.getLogger(__name__)
-
 
 # Database connection and session management
 class Settings(BaseSettings):
-    db_url: str
+    DB_URl: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
@@ -25,13 +23,13 @@ settings = Settings()
 
 # Create an asynchronous engine for the database connection
 async_engine: AsyncEngine = create_async_engine(
-    settings.db_url,
+    settings.DB_URl,
     echo=False,
     connect_args={"timeout": 60, "ssl": True},
 )
 
 # Create an asynchronous session factory for managing database sessions
-async_session_factory = async_sessionmaker(
+async_session_factory = async_sessionmaker[AsyncSession](
     async_engine,
     class_=AsyncSession,
     autoflush=False,
@@ -46,7 +44,7 @@ async def get_session() -> AsyncGenerator[AsyncSession, None]:
         try:
             yield session
         except SQLAlchemyError as e:
-            logger.error(f"Database error: {e}")
+            logging.error(f"Database error: {e}")
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="Database error",
@@ -60,9 +58,9 @@ async def create_db_tables():
     try:
         async with async_engine.begin() as conn:
             await conn.run_sync(SQLModel.metadata.create_all)
-            logger.info("Database tables created successfully.")
+            logging.info("Database tables created successfully.")
     except SQLAlchemyError as e:
-        logger.error(f"Error creating database tables: {e}")
+        logging.error(f"Error creating database tables: {e}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Error creating database tables",
