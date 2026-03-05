10bf2a1ee0c32a4b46d0b0e6b57ceb24007a5b8d
surjanthakur <tsurjan506@gmail.com>
2026-03-05 09:50:54 +0530

    save

--- backend/src/schemas/posts.py
+++ backend/src/schemas/posts.py
@@ -1,21 +1,18 @@
 from pydantic import BaseModel
-from typing import Optional
 from datetime import datetime
 
 
 # Request model for creating a new post
 class PostCreate(BaseModel):
     title: str
-    description: Optional[str]
-    content: str
+    content: dict
     post_type: str
 
 
 # Response model for a single post
 class PostResponse(BaseModel):
     title: str
-    description: Optional[str]
-    content: str
+    content: dict
     post_type: str
     created_at: datetime
 
