df8b980ba06a1e589f70e49a2dfb728587590afc
surjanthakur <tsurjan506@gmail.com>
2026-03-05 17:12:30 +0530

    save

--- frontend/src/services/postServices.js
+++ frontend/src/services/postServices.js
@@ -4,3 +4,20 @@ const API_URL = axios.create({
   baseURL: "http://localhost:8000/api/v1.0/posts",
   withCredentials: true,
 });
+
+const get_all_posts = async () => {
+  const res = await API_URL.get("/all");
+  return res.data;
+};
+
+const createPost = async (data) => {
+  const res = await API_URL.get("/newStory", data);
+  return res.data;
+};
+
+const deletePost = async (post_id) => {
+  const res = await API_URL.delete(`/${post_id}`);
+  return res.data;
+};
+
+export { createPost, deletePost, get_all_posts };
