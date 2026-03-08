46b7077eb8e1eaf0678b451326856f69fd121dc0
surjanthakur <tsurjan506@gmail.com>
2026-03-08 09:15:29 +0530

    save

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from .db.db_connection import create_db_tables
 from fastapi.middleware.cors import CORSMiddleware
 from .router import users_router, posts_router
 import logging
+import time
 
 
 async def lifespan(app: FastAPI):
@@ -19,13 +20,27 @@ app = FastAPI(lifespan=lifespan)
 
 origins = ["http://localhost:5173"]
 
-app.add_middleware(
+
+# cors middleware ------------>
+@app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+# logging middleware ------------->
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start = time.time()
+    response = await call_next(request)
+    duration = time.time() - start
+    logging.info(f"{request.method} {request.url} took {duration:.2f}seconds.🌏")
+
+    return response
+
 
 app.include_router(router=users_router.user_router, prefix="/api/v1.0")
 app.include_router(router=posts_router.post_router, prefix="/api/v1.0")
