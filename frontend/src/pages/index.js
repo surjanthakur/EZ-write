e99b632336b6efae05a098d964b32905950edd69
surjanthakur <tsurjan506@gmail.com>
2026-03-10 00:10:01 +0530

    save

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,7 +2,7 @@ import Homepage from "./HomePage";
 import Login from "./authPages/Login";
 import Signup from "./authPages/signup";
 import AboutUsPage from "./AboutUsPage";
-import WritingPageEditor from "./WritingPage";
+import WritingPageEditor from "./textEditor/WritingPage";
 import PageNotFound from "./pageNotFound";
 import Dashboard from "./dashboard/Dashboard";
 
