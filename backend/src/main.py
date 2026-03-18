3d7575f91b8b2bc54557bd7e2630a1100df60871
surjanthakur <tsurjan506@gmail.com>
2026-03-18 15:06:58 +0530

    save

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,21 +1,29 @@
-from fastapi import FastAPI, Request
-from .db.db_connection import create_db_tables
-from fastapi.middleware.cors import CORSMiddleware
-from .router import users_router, posts_router, ai_router
 import logging
 import time
 
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request
 
+from .db.db_connection import create_db_tables
+from .router import users_router, posts_router, ai_router
+
+
+# function connect to db before starting app
 async def lifespan(app: FastAPI):
     try:
+        # creating db tables
         await create_db_tables()
+        logging.info(msg="db connection successfully 👍🏻🎊")
         yield
+
+    # handle all Exception errors
     except Exception as err:
-        logging.error(f"Error creating database tables: {err}")
+        logging.error(msg=f"Error creating database tables: {err}")
         raise RuntimeError(f"Error creating database tables: {err}")
 
 
-app = FastAPI(lifespan=lifespan)
+# creating app
+app = FastAPI(lifespan=lifespan, title="Inkforge.ai", version="1.0")
 
 origins = ["http://localhost:5173"]
 
@@ -36,10 +44,11 @@ async def log_requests(request: Request, call_next):
     start = time.time()
     response = await call_next(request)
     duration = time.time() - start
-    logging.info(f"{request.method} {request.url} took {duration:.2f}seconds.🌏")
+    logging.info(msg=f"{request.method} {request.url} took {duration:.2f}seconds.🌏")
     return response
 
 
+# all services routers ---------->
 app.include_router(router=users_router.user_router, prefix="/api/v1.0")
 app.include_router(router=posts_router.post_router, prefix="/api/v1.0")
 app.include_router(router=ai_router.aiRouter, prefix="/api/v1.0")
