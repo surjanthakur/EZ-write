78a11683b5d36ffc8fdd759fa9941dd7a8cf911b
surjanthakur <tsurjan506@gmail.com>
2026-03-18 20:55:28 +0530

    d

--- backend/src/router/ai_router.py
+++ backend/src/router/ai_router.py
@@ -1,16 +1,22 @@
 from fastapi import APIRouter, status, Depends, HTTPException
+import logging
+
 from ..service.users_service import current_user
 from ..db.models import User
 from ..service.ai_service import ai_stream_response
 from ..schemas.chatbot import ChatRequest
-from fastapi.responses import StreamingResponse
-import logging
+
 
 aiRouter = APIRouter(tags=["Chatbot"], prefix="/chatbot")
 
 
 # api to generate ai response for req
-@aiRouter.post("/msg/stream", status_code=status.HTTP_200_OK)
+@aiRouter.post(
+    "/msg/stream",
+    status_code=status.HTTP_200_OK,
+    description="Generate an AI-powered response to a user message by streaming interaction to the chatbot. Requires authentication.",
+    response_description="Returns the chatbot's AI response to the provided user message.",
+)
 async def ai_chatbot(
     user_query: ChatRequest,
     curr_user: User = Depends(current_user),
@@ -22,5 +28,5 @@ async def ai_chatbot(
 
         return {"role": "ai", "content": response}
     except Exception as err:
-        logging.error(f"Chatbot error: {err}")
+        logging.error(msg=f"Chatbot error: {err}")
         raise HTTPException(status_code=500, detail="AI response error")
