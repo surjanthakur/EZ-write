0d03149ae83a58141119a98c3a494ca9e1a51ce4
surjanthakur <tsurjan506@gmail.com>
2026-04-05 21:38:21 +0530

    save

--- backend/src/router/google_auth.py
+++ backend/src/router/google_auth.py
@@ -5,8 +5,6 @@ from fastapi.security import OAuth2PasswordRequestForm
 from authlib.integrations.base_client import OAuthError
 from authlib.oauth2.rfc6749 import OAuth2Token
 from authlib.integrations.starlette_client import OAuth
-
-from typing import Annotated
 from dotenv import load_dotenv
 import os
 
@@ -18,17 +16,10 @@ oauth.register(
     name="google",
     client_id=os.getenv("GOOGLE_CLIENT_ID"),
     client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
-    authorize_url="https://accounts.google.com/o/oauth2/auth",
-    access_token_url="https://oauth2.googleapis.com/token",
     client_kwargs={"scope": "openid email profile"},
     server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    redirect_url="http://localhost:8000/api/auth/google/callback",
 )
-
-
-google_client_id = os.getenv("GOOGLE_CLIENT_ID")
-google_client_secret = os.getenv("GOOGLE_CLIENT_SECRET")
-google_redirect_url = os.getenv("GOOGLE_REDIRECT_URL")
-frontend_redirect_url = ""
 
 
 oauth_router = APIRouter(tags=["oauth authentications"], prefix="/auth")
@@ -36,10 +27,5 @@ oauth_router = APIRouter(tags=["oauth authentications"], prefix="/auth")
 
 @oauth_router.get("/google/login")
 async def login_google(req: Request):
-    redirect_url = "https://accounts.google.com/o/oauth2/v2/auth"
-    return await oauth.google.authorize_redirect()
-
-
-@oauth_router.get("/google/callback")
-async def google_callback(req: Request):
-    pass
+    redirect_url = req.url_for("auth")
+    return await oauth.google.authorize_redirect(req, redirect_url)
