171c350f7c8f4fdafec5b6b4761e44fe64da7a65
surjanthakur <tsurjan506@gmail.com>
2026-04-08 01:53:54 +0530

    s

--- backend/src/schemas/chatbot.py
+++ backend/src/schemas/chatbot.py
@@ -1,9 +1,8 @@
 from pydantic import BaseModel
-from typing import Optional
 
 
 class ChatRequest(BaseModel):
-    title: str[Optional]
-    post_type: str[Optional]
-    context: str[Optional]
-    user_query: str[Optional]
+    title: str
+    post_type: str
+    context: str
+    user_query: str
