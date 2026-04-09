66fdd760f65a62e35466fca1fea6d9205354b9ae
surjanthakur <tsurjan506@gmail.com>
2026-04-09 19:57:10 +0530

    save

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000, // or 1500–2000 if needed
+  },
 });
